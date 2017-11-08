b5a895df147c23828b293f33fb88c3063acc4633
ThekhoN <bennetonss@hotmail.com>
2017-11-08 23:06:46 +0530

    added react hot config

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,20 @@
+/*
 function addAll() {
   return [...arguments].reduce((acc, num) => acc + num, 0);
 }
 
 const result = addAll(1, 2, 3, 4);
 console.log("result: ", result);
+*/
+
+import React, { Component } from "react";
+import { render } from "react-dom";
+// import "../css/style.css";
+
+export default class Hello extends Component {
+  render() {
+    return <h2>Hello !!!</h2>;
+  }
+}
+
+render(<Hello />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,79 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
 
+// const PATHS = {
+//   src: path.resolve(__dirname, "src"),
+//   build: path.resolve(__dirname, "dist")
+// };
 const paths = {
   DIST: path.resolve(__dirname, "dist"),
   SRC: path.resolve(__dirname, "src"),
   JS: path.resolve(__dirname, "src/js")
 };
 
-module.exports = {
+/*
+new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, "index.html")
+    })
+*/
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.join(paths.SRC, "index.html")
+});
+
+const ExtractTextPluginConfig = new ExtractTextPlugin({
+  filename: "style.css",
+  disable: false
+});
+
+const base = {
   entry: path.join(paths.JS, "app.js"),
   output: {
     path: paths.DIST,
-    filename: "app.bundle.js"
+    filename: "bundle.js"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(paths.SRC, "index.html")
-    })
-  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
+        // include: path.resolve(__dirname, "src"),
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
       }
+      //   {
+      //     test: /\.(css)$/,
+      //     use: ExtractTextPlugin.extract({
+      //       use: {
+      //         loader: "css-loader",
+      //         options: {
+      //           modules: true,
+      //           localIdentName: "[path][name]__[local]--[hash:base64:5]"
+      //         }
+      //       }
+      //     })
+      //   }
     ]
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
   }
-  //   devServer: {
-  //     contentBase: paths.SRC
-  //   }
 };
+
+const developmentConfig = {
+  devtool: "cheap-module-inline-source-map",
+  devServer: {
+    contentBase: paths.SRC,
+    hot: true,
+    inline: true
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+    ExtractTextPluginConfig,
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+module.exports = Object.assign({}, base, developmentConfig);
